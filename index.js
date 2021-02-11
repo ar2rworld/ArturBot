f9a91bf26a59879e9b4f9d60182410c7b8291dd1
EC2 Default User <ec2-user@ip-172-31-2-7.us-east-2.compute.internal>
2021-02-11 07:09:27 +0000

    init commit 2

--- index.js
+++ index.js
@@ -4,11 +4,17 @@ const config = require('./config.json');
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
-
+const prefix = '!@';
+const help = "Hey my friend, Commands I have for now:\n!@isthebest <name>\n!@number <your number>\n!@match <name1> <age1> <or any other attributes> <name2> <age2> <or any other attributes>\n!@happy-birthday <name>\nThanks to Artur,Aman(they are real sweet hearts)\nv0.01\nhttps://github.com/ar2rworld/ArturBot/blob/master/index.js\n\n\nArtur DELETE THE SERVER SO it does not have all the functionality";
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+  const inp = msg.content.toLowerCase();
+  if(inp.startsWith(prefix)){
+    if(inp.indexOf('help')>0){
+      msg.reply(help);
+    }
+  }
+  if (inp.indexOf('arturbot')>=0){
+    msg.reply(help);
   }
 });
-console.log(config.token);
 client.login(config.token);
