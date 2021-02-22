8a65c2f3eef3b1d40964448bd5b2573613c71577
EC2 Default User <ar2r.world@gmail.com>
2021-02-22 08:02:22 +0000

    meme command added

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const { MessageAttachment } = require('discord.js');
 const config = require('./config.json');
 const fs = require('fs'); //readFile function 
+//const ytdl = require('ytdl-core-discord');
+const ytdl = require("discord-ytdl-core");
 //var StringBuilder = require('stringbuilder');
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const request = require('request')
 
 //Notes:
-//Issue 2: promise request from any website, some attempts in readFileStuff.js 
 //
-
 
 /*Used methods:*/
 const getASCIIsum = (s) =>{
   return s.toLowerCase().split('').reduce( (t, c) => t + c.charCodeAt(0),0);
 }
-
+const download = (url, path, callback) => {
+  //https://flaviocopes.com/node-download-image/
+  try{
+    request.head(url, (err, res, body) => {
+      request(url)
+        .pipe(fs.createWriteStream(path))
+        .on('close', callback).on('error', err => console.log(err))
+    })
+  }catch(err){
+    console.log(err);
+  }
+}
 /*Main functionality*/
 //number game variables:
 let gameStatus = false;
@@ -23,7 +36,7 @@ let attempts = 0;
 
 //bot vars
 const prefix = '!@';
-let help = "Hey my friend, Commands I have for now:\n!@changeAva\n!@isTheBest <name>\n!@number //Guess my number in range of [0, 99]\n->!@number <your number> //replies you if number is bigger or smaller\n->!@number new //new game\n!@match <name1> <name2> [any optinal args]\n!@happy-birthday <name>\n!@autoreply //everytime anyone mentions your in the message, bot replies with default message \n->!@autoreply <on/off> //changes your autoreply status\n->!@autoreply <on/off> <your message> //updates autoreply status and sets messages to provided\n->!@autoreply <your message> //changes your autoreply message and sets status to \"on\"\nv0.02\n\nThanks to Artur,Aman(they are real sweet hearts)\nhttps://github.com/ar2rworld/ArturBot/blob/master/index.js\n";
+let help = "Hey my friend, Commands I have for now:\n!@changeAva\n!@isTheBest <name>\n!@number //Guess my number in range of [0, 99]\n->!@number <your number> //replies you if number is bigger or smaller\n->!@number new //new game\n!@match <name1> <name2> [any optinal args]\n!@happy-birthday <name>\n!@autoreply //everytime anyone mentions your in the message, bot replies with default message \n->!@autoreply <on/off> //changes your autoreply status\n->!@autoreply <on/off> <your message> //updates autoreply status and sets messages to provided\n->!@autoreply <your message> //changes your autoreply message and sets status to \"on\"\n!@meme //sends a meme from local storage\n->!@meme //if picture is attached to the message it will be saved to the local storage\n->!@meme <direct link to an image> //donwloads image from web into the storage, supports many links separated by single SPACE\nv0.02\n\nThanks to Artur,Aman(they are real sweet hearts)\nhttps://github.com/ar2rworld/ArturBot/blob/master/index.js\n";
 let autoreplyFileExists = false;
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -48,6 +61,13 @@ client.on('ready', () => {
           }
         });
   }
+  //memes directory check:
+  fs.access("memes", err => {
+      if(err){
+        fs.mkdirSync("memes");
+        console.log("memes folder created");
+      }});
+
 });
 
 
@@ -118,20 +138,42 @@ client.on('message', msg => {
         msg.reply("Invalid input: !@happy-birthday <name>");
       }
     }else if(inp.indexOf("play")>0){
-        /*
-        console.log(client.channels.cache.filter(c => c.type === "voice").array()[0].id);
-        const v0 = client.channels.cache.filter(c => c.type === "voice").array()[0];
-        const channel = v0;
-        if (!channel) return console.error("The channel does not exist!");
+        //console.log(msg.author.client.channels);
+        //console.log(client.channels.cache.filter(c => c.type === "voice").array()[0].id);
+        const v0 = client.channels.cache.filter(c => c.type === "voice").array();
+        const channel1 = v0.filter(c => c.name === "bot-testing")[0];
+        
+        //check for permission
+        //https://github.com/iCrawl/discord-music-bot
+        const { channel } = msg.member.voice;
+    		if (!channel) return msg.channel.send('I\'m sorry but you need to be in a voice channel to play music!');
+		    const permissions = channel.permissionsFor(msg.client.user);
+		    if (!permissions.has('CONNECT')) return msg.channel.send('I cannot connect to your voice channel, make sure I have the proper permissions!');
+		    if (!permissions.has('SPEAK')) return msg.channel.send('I cannot speak in this voice channel, make sure I have the proper permissions!');
+
+        if (!channel1) return console.error("The channel does not exist!");
           channel.join().then(connection => {
           // Yay, it worked!
           console.log("Successfully connected.");
-          connection.play("./21pilots_heathens.mp3");
+          const broadcast = client.voice.createBroadcast();
+          broadcast.play("./ded_maxim.mp3");
+          //connection.play(broadcast);
+          connection.play(ytdl('https://www.youtube.com/watch?v=ZlAU_w7-Xp8', {
+filter: "audioonly",
+opusEndcoded: true
+                }), { type: 'opus' });
+          connection.on("debug" , m =>{
+                console.log("d " + m);
+                });
+          connection.on("error" , m =>{
+                console.log("er " + m);
+                });
         }).catch(e => {
           // Oh no, it errored! Let's log it to console :)
           console.error(e);
         });
-        */
+
+     
     }else if(inp.indexOf("autoreply")>0){
       console.log(msg.author.id + " " + msg.author.username);
       if(autoreplyFileExists){
@@ -176,10 +218,53 @@ client.on('message', msg => {
                 out = err;
               }
             });
-        msg.reply((out?out : "You autoreply is working now without errors"));
+        if(out){
+          msg.reply("Errors occured while writing .json to file:\n" + out);
+        }
       }else{
         msg.reply("autoreply function is not available, as file does not exist");
       }
+    }else if(inp.indexOf("meme")>0){
+      if(msg.attachments.array().length>0 || inp.split(" ").length > 1){
+      const atts = msg.attachments.array();
+      if(atts.length > 0){
+        //console.log(atts);
+        for(let i=0; i<atts.length; i+=1){
+          //console.log(atts[i].name + " -> " +  atts[i].attachment);
+          let url = atts[i].proxyURL;
+          download(url, config.memes+postfix+atts[i].name, () => {//config.memes+postfix+atts[i].name, 
+            msg.channel.send("GOt it");
+          });
+        }
+      }else{
+        msg.channel.send("No imgs attached =(");
+      }
+      const links = msg.content.split(" ").slice(1);
+      for(let i=0; i<links.length; i+=1){
+          let url = links[i];
+          let urlParts = url.split('/');
+          download(url, config.memes  +urlParts[urlParts.length-1] , () => {//config.memes+postfix+atts[i].name, 
+            msg.channel.send("GOt it from link in the message");
+          });
+      }
+      }else{
+        //console.log(msg.attachments.array()[0].attachment);
+        fs.readdir(config.memes, (err, files) => {
+            if(err){
+              console.log(err);
+              msg.channel.send("Error while dir reading");
+            }else{
+              if(files.length > 0){
+                const att = new MessageAttachment(config.memes + files[Math.floor(Math.random() * files.length)]);
+                msg.channel.send(att);
+              }else{
+                msg.channel.send("No files found in dir");
+              }
+            }
+            });
+      }
+      //console.log(msg.attachments.array());
+      //node -e 'const fs = require("fs");fs.writeFile("1.jpg", "https://media.discordapp.net/attachments/812960123265089538/813263670058024970/d.jpg", err => {console.log(err)});'
     }//next command
     
   }
